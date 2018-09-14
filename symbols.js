660df533e0e9a59da450898f2ce80f284319d0d7
mmfKupl <ikuplevich@mail.ru>
2018-09-14 18:25:22 +0300

    update filter

--- symbols.js
+++ symbols.js
@@ -1,11 +1,12 @@
-let express = require('express');
-let Mongo = require('./Mongo');
+const express = require('express')
+const Mongo = require('./Mongo')
 const CONSTANTS = require('./constants')
-var router = express.Router();
+const collection = CONSTANTS.TRADING_SIGNALS_COLLECTION
+var router = express.Router()
 
 router.get('/api/tradeSignals/getData', (req, res, next) => {
 	console.log('/api/tradeSignals/getData')
-	Mongo.select({}, CONSTANTS.TRADING_SIGNALS_COLLECTION, data => {
+	Mongo.select({}, collection, data => {
 		setTimeout(() => { res.send(data) }, 5000)
 	})
 })
@@ -13,13 +14,27 @@ router.get('/api/tradeSignals/getData', (req, res, next) => {
 router.post('/api/tradeSignals/postData', (req, res, next) => {
 	console.log('/api/tradeSignals/postData')
 	let data = req.body
-	Mongo.updateMany({}, data, CONSTANTS.TRADING_SIGNALS_COLLECTION, data => {
-		setTimeout(() => {
-			res.send({
-				status: 'ok',
-				data: data
-			})
-		}, 5000)
+	
+	Mongo.select({}, collection, signals => {
+		// нужно проверить не удалены ли какие-то поля
+		// signals = signals
+		console.log(signals)
+		let nextData = []
+		data.forEach(elem => {
+			let pos = signals.find(findElem => elem.id === findElem.id)
+			if(pos) nextData.push(elem)
+		})
+
+		Mongo.updateMany({}, nextData, collection, resData => {
+			setTimeout(() => {
+				res.send({
+					status: 'ok',
+					data: resData
+				})
+			}, 5000)
+		})
 	})
+
+
 })
 module.exports = router;
