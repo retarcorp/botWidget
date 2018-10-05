765a342a3569529532ac5273243676114423863f
trbots-dev <dev@retarcorp.com>
2018-10-05 14:44:35 +0000

    upd

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: 'chrome-extension://*',
+  origin: 'chrome-extension://fojbjchgejaolhpebfjfkifmgafmaehg',
   credentials: true
 }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'chrome-extension://*');
+  res.header('Access-Control-Allow-Origin', 'chrome-extension://fojbjchgejaolhpebfjfkifmgafmaehg');
   res.header('Access-Control-Allow-Credentials', 'true')
   next();
 })
